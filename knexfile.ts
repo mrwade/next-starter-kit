3a86818b2ae74bca81b4e32f9141045ea386fe95
Kevin Wade <kevinrwade@gmail.com>
2023-06-08 18:37:54 -0500

    Use POSTGRES_URL for knex

--- knexfile.ts
+++ knexfile.ts
@@ -5,7 +5,7 @@ import { Knex } from 'knex';
 const config: Knex.Config = {
   client: 'pg',
   connection:
-    process.env.POSTGRES_PRISMA_URL +
+    process.env.POSTGRES_URL +
     (process.env.NODE_ENV === 'development' ? '' : '?sslmode=require'),
   migrations: {
     extension: 'ts',
