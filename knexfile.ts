10b36dd36d5b5363ddfa143e576460b5eeab5c80
Kevin Wade <kevinrwade@gmail.com>
2023-06-08 18:35:15 -0500

    Use "?sslmode=require" in production

--- knexfile.ts
+++ knexfile.ts
@@ -4,7 +4,9 @@ import { Knex } from 'knex';
 
 const config: Knex.Config = {
   client: 'pg',
-  connection: process.env.POSTGRES_PRISMA_URL,
+  connection:
+    process.env.POSTGRES_PRISMA_URL +
+    (process.env.NODE_ENV === 'development' ? '' : '?sslmode=require'),
   migrations: {
     extension: 'ts',
   },
