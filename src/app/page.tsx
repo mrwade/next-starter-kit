3265cd4abb6da61ec7a0bf73025ba934c85ea939
Kevin Wade <kevinrwade@gmail.com>
2023-06-15 17:41:34 -0500

    Simplify call to server action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Button from '@/components/Button';
 import db from '@/modules/db';
 import { faker } from '@faker-js/faker';
 import { revalidatePath } from 'next/cache';
@@ -21,12 +20,11 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-start p-24">
-      <Button
-        className="bg-black disabled:bg-slate-600 text-white p-5 mb-5 rounded-lg"
-        onClick={generatePosts}
-      >
-        Generate Posts
-      </Button>
+      <form action={generatePosts}>
+        <button className="bg-black text-white p-5 mb-5 rounded-lg">
+          Generate Posts
+        </button>
+      </form>
 
       {posts.map((post) => (
         <div key={post.id} className="bg-white mb-3 px-5 py-3 rounded-md">
